5c4fbace948daf7391235d70e89e0fd833987dbe
LONG Kongheng <kongheng.long@sbilhbank.com.kh>
2024-03-12 16:45:48 +0700

    prevent null

--- CoreBankingService/src/main/java/kh/com/sbilhbank/cbs/controller/CoreBankingController.java
+++ CoreBankingService/src/main/java/kh/com/sbilhbank/cbs/controller/CoreBankingController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/core")
 public class CoreBankingController {
@@ -27,20 +29,27 @@ public class CoreBankingController {
     @GetMapping("user/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id,
         @RequestParam("Authorization") String accessToken) throws Exception {
-        authenticationService.validate(accessToken);
-        CardInfo cardInfo = cardSystemService.getCardInfoByUserId(id, accessToken).getBody();
-        User user = coreBankingService.getUserById(id);
-        UserResponse userResponse = UserResponse.builder()
-            .id(user.getId())
-            .name(user.getName())
-            .gender(user.getGender())
-            .address(user.getAddress())
-            .cardId(cardInfo.getId())
-            .cardName(cardInfo.getName())
-            .cvv(cardInfo.getCvv())
-            .createDate(cardInfo.getCreateDate())
-            .expireDate(cardInfo.getExpireDate())
-            .build();
+        UserResponse userResponse = new UserResponse();
+        try {
+            authenticationService.validate(accessToken);
+            CardInfo cardInfo = cardSystemService.getCardInfoByUserId(id, accessToken).getBody();
+            User user = coreBankingService.getUserById(id);
+            if (Objects.nonNull(cardInfo) && Objects.nonNull(user)) {
+                userResponse = UserResponse.builder()
+                    .id(user.getId())
+                    .name(user.getName())
+                    .gender(user.getGender())
+                    .address(user.getAddress())
+                    .cardId(cardInfo.getId())
+                    .cardName(cardInfo.getName())
+                    .cvv(cardInfo.getCvv())
+                    .createDate(cardInfo.getCreateDate())
+                    .expireDate(cardInfo.getExpireDate())
+                    .build();
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(userResponse, HttpStatus.NO_CONTENT);
+        }
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 }
